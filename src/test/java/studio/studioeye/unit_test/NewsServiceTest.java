1b6f52b4431248a7c3bce163cf6a029e63efd567
ibaesuyeon <ibaesuyeon@Naver.com>
2024-10-14 03:45:15 +0900

    [TEST] 뉴스 생성 실패 테스트

--- src/test/java/studio/studioeye/unit_test/NewsServiceTest.java
+++ src/test/java/studio/studioeye/unit_test/NewsServiceTest.java
@@ -52,4 +52,25 @@ public class NewsServiceTest {
         assertEquals("News를 성공적으로 등록하였습니다.", response.getMessage());
     }
 
+    @Test
+    @DisplayName("뉴스 생성 실패 - 타이틀이 비어있는 경우")
+    public void createNews_fail_emptyTitle() {
+        // given: 타이틀 필드가 비어있는 DTO를 생성
+        CreateNewsServiceRequestDto requestDto = new CreateNewsServiceRequestDto(
+                "", // title이 비어있음
+                "source",
+                LocalDate.of(2024, 1, 1),
+                "https://www.naver.com/",
+                true
+        );
+
+        // when: NewsService의 createNews 메서드를 호출
+        ApiResponse<News> response = newsService.createNews(requestDto);
+
+        // then: 오류 응답이 반환되는지 확인
+        assertNotNull(response);
+        assertEquals(ErrorCode.NEWS_IS_EMPTY.getStatus(), response.getStatus()); // 에러 코드 검증
+        assertEquals(ErrorCode.NEWS_IS_EMPTY.getMessage(), response.getMessage()); // 에러 메시지 검증
+    }
+
 }
