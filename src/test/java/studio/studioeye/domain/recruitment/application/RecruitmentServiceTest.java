7c011e6cc28eebe4999e2dbaf724378e8ac6df1f
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-21 16:14:43 +0900

    [TEST] Recruitment 페이지네이션 조회 성공 테스트

--- src/test/java/studio/studioeye/domain/recruitment/application/RecruitmentServiceTest.java
+++ src/test/java/studio/studioeye/domain/recruitment/application/RecruitmentServiceTest.java
@@ -7,23 +7,28 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import studio.studioeye.domain.news.domain.News;
-import studio.studioeye.domain.news.dto.CreateNewsServiceRequestDto;
 import studio.studioeye.domain.recruitment.dao.RecruitmentRepository;
+import studio.studioeye.domain.recruitment.dao.RecruitmentTitle;
 import studio.studioeye.domain.recruitment.domain.Recruitment;
+import studio.studioeye.domain.recruitment.domain.Status;
 import studio.studioeye.domain.recruitment.dto.request.CreateRecruitmentServiceRequestDto;
 import studio.studioeye.global.common.response.ApiResponse;
 import studio.studioeye.global.exception.error.ErrorCode;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
@@ -107,5 +112,95 @@ public class RecruitmentServiceTest {
     }
 
     @Test
-    @DisplayName("채용공고 실패 테스트 - 시작일이 마감일보다 이후인 경우")
+    @DisplayName("채용공고 페이지네이션 조회 성공 테스트")
+    public void retrieveRecruitmentListSuccess() {
+        int page = 0;
+        int size = 2;
+
+        Pageable pageable = PageRequest.of(page, size);
+
+
+        List<RecruitmentTitle> recruitmentList = new ArrayList<>();
+        recruitmentList.add(new RecruitmentTitle() {
+            @Override
+            public Long getId() {
+                return 1L;
+            }
+
+            @Override
+            public String getTitle() {
+                return "Test Title1";
+            }
+
+            @Override
+            public Status getStatus() {
+                return Status.OPEN;
+            }
+        });
+        recruitmentList.add(new RecruitmentTitle() {
+            @Override
+            public Long getId() {
+                return 2L;
+            }
+
+            @Override
+            public String getTitle() {
+                return "Test Title2";
+            }
+
+            @Override
+            public Status getStatus() {
+                return Status.OPEN;
+            }
+        });
+        recruitmentList.add(new RecruitmentTitle() {
+            @Override
+            public Long getId() {
+                return 3L;
+            }
+
+            @Override
+            public String getTitle() {
+                return "Test Title3";
+            }
+
+            @Override
+            public Status getStatus() {
+                return Status.OPEN;
+            }
+        });
+        recruitmentList.add(new RecruitmentTitle() {
+            @Override
+            public Long getId() {
+                return 4L;
+            }
+
+            @Override
+            public String getTitle() {
+                return "Test Title4";
+            }
+
+            @Override
+            public Status getStatus() {
+                return Status.OPEN;
+            }
+        });
+
+
+        Page<RecruitmentTitle> recruitmentTitlePage = new PageImpl<>(recruitmentList, pageable, recruitmentList.size());
+
+        // stub
+        when(recruitmentRepository.findAllRecruitments(pageable)).thenReturn(recruitmentTitlePage);
+
+        // when
+        ApiResponse<Page<RecruitmentTitle>> response = recruitmentService.retrieveRecruitmentList(page, size);
+
+        // then
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("채용공고 목록을 성공적으로 조회했습니다.", response.getMessage());
+        assertEquals(recruitmentTitlePage, response.getData());
+        Mockito.verify(recruitmentRepository, times(1)).findAllRecruitments(pageable);
+    }
 }
