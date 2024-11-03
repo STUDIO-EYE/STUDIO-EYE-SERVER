8a8140aab738cdc4b5a38a0cef2666b8e232093e
MoonInbae <mib010213@gmail.com>
2024-11-04 06:13:12 +0900

    [TEST]: 조회수 생성 실패 테스트 - 이미 해당 연월의 조회수가 존재하는 경우

--- src/test/java/studio/studioeye/domain/views/application/ViewsServiceTest.java
+++ src/test/java/studio/studioeye/domain/views/application/ViewsServiceTest.java
@@ -13,6 +13,7 @@ import studio.studioeye.domain.project.domain.ArtworkCategory;
 import studio.studioeye.domain.recruitment.application.RecruitmentService;
 import studio.studioeye.domain.recruitment.dao.RecruitmentRepository;
 import studio.studioeye.domain.recruitment.domain.Recruitment;
+import studio.studioeye.domain.recruitment.domain.Status;
 import studio.studioeye.domain.recruitment.dto.request.CreateRecruitmentServiceRequestDto;
 import studio.studioeye.domain.views.dao.ViewsRepository;
 import studio.studioeye.domain.views.domain.Views;
@@ -20,11 +21,13 @@ import studio.studioeye.domain.views.dto.request.CreateViewsServiceRequestDto;
 import studio.studioeye.global.common.response.ApiResponse;
 import studio.studioeye.global.exception.error.ErrorCode;
 
+import java.util.Date;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ViewsServiceTest {
@@ -81,6 +84,41 @@ public class ViewsServiceTest {
         assertNotNull(response);
         assertEquals(ErrorCode.INVALID_VIEWS_MONTH.getStatus(), response.getStatus());
         assertEquals(ErrorCode.INVALID_VIEWS_MONTH.getMessage(), response.getMessage());
+        Mockito.verify(viewsRepository, never()).save(any(Views.class));
+    }
+
+    @Test
+    @DisplayName("조회수 생성 실패 테스트 - 이미 해당 연월의 조회수가 존재하는 경우")
+    public void createViewsFail_alreadyExisted() {
+        //given
+        Integer year = 2024;
+        Integer month = 11;
+        Long views = 1L;
+        MenuTitle menu = MenuTitle.ABOUT;
+        ArtworkCategory category = ArtworkCategory.ALL;
+
+        Views savedViews = Views.builder()
+                .year(year)
+                .month(month)
+                .views(views)
+                .menu(menu)
+                .category(category)
+                .build();
+
+        CreateViewsServiceRequestDto requestDto = new CreateViewsServiceRequestDto(
+                year, month, views, menu, category
+        );
+
+        // stub
+        when(viewsRepository.findByYearAndMonth(year, month)).thenReturn(Optional.of(savedViews));
+
+        //when
+        ApiResponse<Views> response = viewsService.createViews(requestDto);
+
+        //then
+        assertNotNull(response);
+        assertEquals(ErrorCode.ALREADY_EXISTED_DATA.getStatus(), response.getStatus());
+        assertEquals(ErrorCode.ALREADY_EXISTED_DATA.getMessage(), response.getMessage());
         Mockito.verify(viewsRepository, never()).save(any(Views.class));
     }
 }
