f9a230892027b619b97e371ef9f13cf49ab6acd7
MoonInbae <mib010213@gmail.com>
2024-11-04 05:56:33 +0900

    [TEST]: 조회수 생성 실패 테스트 - 유효하지 않은 월 형식(1~12)인 경우

--- src/test/java/studio/studioeye/domain/views/application/ViewsServiceTest.java
+++ src/test/java/studio/studioeye/domain/views/application/ViewsServiceTest.java
@@ -18,10 +18,12 @@ import studio.studioeye.domain.views.dao.ViewsRepository;
 import studio.studioeye.domain.views.domain.Views;
 import studio.studioeye.domain.views.dto.request.CreateViewsServiceRequestDto;
 import studio.studioeye.global.common.response.ApiResponse;
+import studio.studioeye.global.exception.error.ErrorCode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,5 +57,30 @@ public class ViewsServiceTest {
         assertEquals(HttpStatus.OK, response.getStatus());
         assertEquals("조회 수 등록을 완료했습니다.", response.getMessage());
         Mockito.verify(viewsRepository, times(1)).save(any(Views.class));
+    }
+
+    @Test
+    @DisplayName("조회수 생성 실패 테스트 - 유효하지 않은 월 형식(1~12)인 경우")
+    public void createViewsFail_invalidMonth() {
+        //given
+        Integer year = 2024;
+        Integer month = 13;
+        Long views = 1L;
+        MenuTitle menu = MenuTitle.ABOUT;
+        ArtworkCategory category = ArtworkCategory.ALL;
+
+
+        CreateViewsServiceRequestDto requestDto = new CreateViewsServiceRequestDto(
+                year, month, views, menu, category
+        );
+
+        //when
+        ApiResponse<Views> response = viewsService.createViews(requestDto);
+
+        //then
+        assertNotNull(response);
+        assertEquals(ErrorCode.INVALID_VIEWS_MONTH.getStatus(), response.getStatus());
+        assertEquals(ErrorCode.INVALID_VIEWS_MONTH.getMessage(), response.getMessage());
+        Mockito.verify(viewsRepository, never()).save(any(Views.class));
     }
 }
