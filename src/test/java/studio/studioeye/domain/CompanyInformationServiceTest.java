e68187f7ccc1e452b8abc014861994b952a9ccda
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-11-11 17:42:27 +0900

    [TEST]: 회사 소개 정보(mainOverview, commitment, introduction) 수정 실패 테스트

--- src/test/java/studio/studioeye/domain/CompanyInformationServiceTest.java
+++ src/test/java/studio/studioeye/domain/CompanyInformationServiceTest.java
@@ -922,4 +922,47 @@ public class CompanyInformationServiceTest {
         assertEquals("회사 소개 정보를 성공적으로 수정했습니다.", response.getMessage());
         Mockito.verify(companyInformationRepository, times(1)).save(any(CompanyInformation.class));
     }
+
+    @Test
+    @DisplayName("회사 소개 정보(mainOverview, commitment, introduction) 수정 실패 테스트")
+    public void updateCompanyIntroductionInformationFail() {
+        // given
+        String mainOverview = "Test mainOverview";
+        String commitment = "Test commitment";
+        String introduction = "Test introduction";
+
+        List<DetailInformationDTO> detailInformation = new ArrayList<>();
+
+        DetailInformationDTO dto1 = new DetailInformationDTO();
+        dto1.setKey("Test Key1");
+        dto1.setValue("Test Value1");
+        detailInformation.add(dto1);
+
+        DetailInformationDTO dto2 = new DetailInformationDTO();
+        dto2.setKey("Test Key2");
+        dto2.setValue("Test Value2");
+        detailInformation.add(dto2);
+
+        DetailInformationDTO dto3 = new DetailInformationDTO();
+        dto3.setKey("Test Key3");
+        dto3.setValue("Test Value3");
+        detailInformation.add(dto3);
+
+        UpdateCompanyIntroductionInformationServiceRequestDto requestDto = new UpdateCompanyIntroductionInformationServiceRequestDto(
+                mainOverview, commitment, introduction
+        );
+
+        // stub
+        when(companyInformationRepository.findAll()).thenReturn(List.of());
+
+        // when
+        ApiResponse<CompanyInformation> response = companyInformationService.updateCompanyIntroductionInformation(requestDto);
+
+        // then
+        assertNotNull(response);
+        assertEquals(ErrorCode.COMPANYINFORMATION_IS_EMPTY.getStatus(), response.getStatus());
+        assertEquals(ErrorCode.COMPANYINFORMATION_IS_EMPTY.getMessage(), response.getMessage());
+        Mockito.verify(companyInformationRepository, times(1)).findAll();
+        Mockito.verify(companyInformationRepository, never()).save(any(CompanyInformation.class));
+    }
 }
