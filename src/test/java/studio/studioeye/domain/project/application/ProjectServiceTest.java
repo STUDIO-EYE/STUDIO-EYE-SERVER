802f737fa0c5522d64ded28e027cbe01d8dc7ce5
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-11-18 17:58:50 +0900

    [TEST]: Main Page 프로젝트 순서 변경 성공 테스트

--- src/test/java/studio/studioeye/domain/project/application/ProjectServiceTest.java
+++ src/test/java/studio/studioeye/domain/project/application/ProjectServiceTest.java
@@ -1004,4 +1004,86 @@ public class ProjectServiceTest {
         assertEquals(ErrorCode.INVALID_PROJECT_ID.getStatus(), response.getStatus());
         assertEquals(ErrorCode.INVALID_PROJECT_ID.getMessage(), response.getMessage());
     }
+
+    @Test
+    @DisplayName("Main Page 프로젝트 순서 변경 성공 테스트")
+    void changeMainSequenceProjectSuccess() {
+        // given
+        Project project1 = Project.builder()
+                .name("Test Name")
+                .category("Entertainment")
+                .department("Test Department")
+                .date("2024-01-01")
+                .link("Test Link")
+                .overView("Test Overview")
+                .isPosted(true)
+                .projectType("main")
+                .mainImg("test url")
+                .mainImgFileName(mockFile.getName())
+                .responsiveMainImg("test url")
+                .responsiveMainImgFileName(mockFile.getName())
+                .mainSequence(1)
+                .sequence(0)
+                .build();
+        Project project2 = Project.builder()
+                .name("Test Name")
+                .category("Entertainment")
+                .department("Test Department")
+                .date("2024-01-01")
+                .link("Test Link")
+                .overView("Test Overview")
+                .isPosted(true)
+                .projectType("main")
+                .mainImg("test url")
+                .mainImgFileName(mockFile.getName())
+                .responsiveMainImg("test url")
+                .responsiveMainImgFileName(mockFile.getName())
+                .mainSequence(1)
+                .sequence(0)
+                .build();
+        Project project3 = Project.builder()
+                .name("Test Name")
+                .category("Entertainment")
+                .department("Test Department")
+                .date("2024-01-01")
+                .link("Test Link")
+                .overView("Test Overview")
+                .isPosted(true)
+                .projectType("main")
+                .mainImg("test url")
+                .mainImgFileName(mockFile.getName())
+                .responsiveMainImg("test url")
+                .responsiveMainImgFileName(mockFile.getName())
+                .mainSequence(1)
+                .sequence(0)
+                .build();
+
+        List<ChangeMainSequenceProjectReq> changeMainSequenceProjectReqList = new ArrayList<>();
+        ChangeMainSequenceProjectReq req1 = new ChangeMainSequenceProjectReq();
+        req1.setProjectId(0L);
+        req1.setMainSequence(1);
+        changeMainSequenceProjectReqList.add(req1);
+        ChangeMainSequenceProjectReq req2 = new ChangeMainSequenceProjectReq();
+        req2.setProjectId(1L);
+        req2.setMainSequence(2);
+        changeMainSequenceProjectReqList.add(req2);
+        ChangeMainSequenceProjectReq req3 = new ChangeMainSequenceProjectReq();
+        req3.setProjectId(2L);
+        req3.setMainSequence(0);
+        changeMainSequenceProjectReqList.add(req3);
+
+        // stub
+        when(projectRepository.findById(req1.getProjectId())).thenReturn(Optional.of(project1));
+        when(projectRepository.findById(req2.getProjectId())).thenReturn(Optional.of(project2));
+        when(projectRepository.findById(req3.getProjectId())).thenReturn(Optional.of(project3));
+
+        // when
+        ApiResponse<String> response = projectService.changeMainSequenceProject(changeMainSequenceProjectReqList);
+
+        // then
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("메인 페이지에 보여질 프로젝트의 순서를 성공적으로 수정하였습니다.", response.getMessage());
+        Mockito.verify(projectRepository, times(3)).findById(any(Long.class));
+    }
+
 }
