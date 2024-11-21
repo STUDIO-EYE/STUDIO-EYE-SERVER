63909fc8617787e86ad2f3b63250191784054677
8create8 <limjeanne99@gmail.com>
2024-11-22 00:29:30 +0900

    [Fix]: emailService.sendEmail() 메서드 중복 호출 삭제
    RequestService.createRequest()에서 emailService.sendEmail() 메서드 호출을 한 번만 수행하도록 수정

--- src/main/java/studio/studioeye/domain/request/application/RequestService.java
+++ src/main/java/studio/studioeye/domain/request/application/RequestService.java
@@ -108,12 +108,12 @@ public class RequestService {
 				+ "프로젝트명: " + savedRequest.getProjectName() + "\n"
 				+ "문의 내용: " + savedRequest.getDescription() + "\n";
 
-		emailService.sendEmail(savedRequest.getEmail(), subject, text);
 		boolean isExceeded = emailService.sendEmail(savedRequest.getEmail(), subject, text);
-		if(!isExceeded) {
+		if (!isExceeded) {
 			return ApiResponse.withError(ErrorCode.EMAIL_SIZE_EXCEEDED);
 		}
-		notificationService.subscribe(request.getId());    // 문의 등록 알림 보내기
+
+		notificationService.subscribe(request.getId()); // 문의 등록 알림 보내기
 		return ApiResponse.ok("문의를 성공적으로 등록하였습니다.", savedRequest);
 	}
 
