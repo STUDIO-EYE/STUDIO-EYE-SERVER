602b7fcaa61004457bfea4c161a0a6542a2fa0bf
8create8 <limjeanne99@gmail.com>
2024-12-02 02:04:18 +0900

    [Refactor]: Status 주석 코드 삭제

--- src/main/java/studio/studioeye/domain/recruitment/domain/Status.java
+++ src/main/java/studio/studioeye/domain/recruitment/domain/Status.java
@@ -8,9 +8,6 @@ public enum Status {
     OPEN("진행중"),
     CLOSE("마감");
     public final String label;
-    //    private Status(String label) {
-//        this.label = label;
-//    }
     public String getLabel() {
         return this.label;
     }
