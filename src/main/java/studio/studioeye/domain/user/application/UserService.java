fafa92f4b0123491f6ee9d2d124545431623da2d
8create8 <limjeanne99@gmail.com>
2024-12-02 00:43:24 +0900

    [Refactor]: UserService Unused Import 제거

--- src/main/java/studio/studioeye/domain/user/application/UserService.java
+++ src/main/java/studio/studioeye/domain/user/application/UserService.java
@@ -1,7 +1,6 @@
 package studio.studioeye.domain.user.application;
 
 import org.springframework.http.ResponseEntity;
-import studio.studioeye.domain.user.domain.User;
 import studio.studioeye.domain.user.dto.request.RequestLogin;
 import studio.studioeye.domain.user.dto.request.RequestUser;
 import studio.studioeye.domain.user.dto.response.EmailVerificationResult;
@@ -9,7 +8,6 @@ import studio.studioeye.domain.user.dto.response.JWTAuthResponse;
 import studio.studioeye.domain.user.dto.response.UserResponse;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService{
 
