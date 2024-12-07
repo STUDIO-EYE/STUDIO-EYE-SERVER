932e5567ee2da470ebdb979ef67150556a6cd953
8create8 <limjeanne99@gmail.com>
2024-12-07 21:22:04 +0900

    [Refactor]: StudioEyeApplicationTests - 주석제거와 임포트 제거

--- src/test/java/studio/studioeye/StudioEyeApplicationTests.java
+++ src/test/java/studio/studioeye/StudioEyeApplicationTests.java
@@ -1,13 +1,7 @@
 package studio.studioeye;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudioEyeApplicationTests {
-
-//    @Test
-//    void contextLoads() {
-//    }
-
 }
