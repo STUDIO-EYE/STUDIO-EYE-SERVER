68da054da61b19ed01e0542f5a51fc986522729f
8create8 <limjeanne99@gmail.com>
2024-12-08 00:07:55 +0900

    [Refactor]: NewsServiceTest - blank

--- src/test/java/studio/studioeye/domain/news/application/NewsServiceTest.java
+++ src/test/java/studio/studioeye/domain/news/application/NewsServiceTest.java
@@ -34,22 +34,17 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class NewsServiceTest {
-
     @InjectMocks
     private NewsService newsService;
-
     @Mock
     private NewsRepository newsRepository;
-
     @Test
     @DisplayName("News 페이지네이션 조회 성공 테스트")
     void retrieveNewsPageSuccess() {
         // given
         int page = 0;
         int size = 2;
-
         Pageable pageable = PageRequest.of(page, size);
-
         List<News> newsList = new ArrayList<>();
         newsList.add(new News("Test Title1", "Test Source1",  LocalDate.now(), "Test URL1", true));
         newsList.add(new News("Test Title2", "Test Source2",  LocalDate.now(), "Test URL2", true));
@@ -61,15 +56,11 @@ class NewsServiceTest {
         newsList.add(new News("Test Title8", "Test Source8",  LocalDate.now(), "Test URL8", true));
         newsList.add(new News("Test Title9", "Test Source9",  LocalDate.now(), "Test URL9", true));
         newsList.add(new News("Test Title10", "Test Source10",  LocalDate.now(), "Test URL10", true));
-
         Page<News> newsPage = new PageImpl<>(newsList, pageable, newsList.size());
-
         // stub
         when(newsRepository.findAll(pageable)).thenReturn(newsPage);
-
         // when
         Page<News> result = newsService.retrieveNewsPage(page, size);
-
         // then
         Assertions.assertThat(result)
                 .isNotNull()
@@ -82,14 +73,11 @@ class NewsServiceTest {
         // given
         int invalidPage = -1;
         int invalidSize = 0;
-
         // when & then
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             newsService.retrieveNewsPage(invalidPage, invalidSize);
         });
-
         Assertions.assertThat(exception.getMessage()).isEqualTo("Page index must not be less than zero");
-
         // 리포지토리가 호출되지 않았는지 확인
         Mockito.verify(newsRepository, Mockito.never()).findAll(any(Pageable.class));
     }
@@ -101,17 +89,13 @@ class NewsServiceTest {
         int page = 0;
         int size = 2;
         Pageable pageable = PageRequest.of(page, size);
-
         // when
         when(newsRepository.findAll(pageable)).thenThrow(new RuntimeException("Database Error"));
-
         // then
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
             newsService.retrieveNewsPage(page, size);
         });
-
         Assertions.assertThat(exception.getMessage()).isEqualTo("Database Error");
-
         Mockito.verify(newsRepository, times(1)).findAll(pageable);
     }
 
@@ -127,13 +111,10 @@ class NewsServiceTest {
         Boolean newVisibility = false;
         News savedNews = new News("Test Title1", "Test Source1", LocalDate.of(2022, 1, 1), "Test URL1", true);
         UpdateNewsServiceRequestDto dto = new UpdateNewsServiceRequestDto(id, newTitle, newSource, newPubDate, newUrl, newVisibility);
-
         // stub
         when(newsRepository.findById(id)).thenReturn(Optional.of(savedNews));
-
         // when
         ApiResponse<News> response = newsService.updateNews(dto);
-
         // then
         Assertions.assertThat(response.getMessage()).isEqualTo("News를 성공적으로 수정하였습니다.");
         Assertions.assertThat(newTitle).isEqualTo(savedNews.getTitle());
@@ -141,7 +122,6 @@ class NewsServiceTest {
         Assertions.assertThat(newPubDate).isEqualTo(savedNews.getPubDate());
         Assertions.assertThat(newUrl).isEqualTo(savedNews.getUrl());
         Assertions.assertThat(newVisibility).isEqualTo(savedNews.getVisibility());
-
         Mockito.verify(newsRepository, times(1)).findById(id);
     }
 
@@ -152,14 +132,11 @@ class NewsServiceTest {
         Long invalidId = 999L;
         News savedNews = new News("Test Title1", "Test Source1", LocalDate.of(2022, 1, 1), "Test URL1", true);
         UpdateNewsServiceRequestDto dto = new UpdateNewsServiceRequestDto(invalidId, "Title", "Source", LocalDate.now(), "https://url.com", true);
-
         // stub
         when(newsRepository.findById(invalidId)).thenReturn(Optional.empty());
-
         // when
         ApiResponse<News> response = newsService.updateNews(dto);
         News findNews = response.getData();
-
         // then
         Assertions.assertThat(response.getStatus()).isEqualTo(ErrorCode.INVALID_NEWS_ID.getStatus());
         Assertions.assertThat(findNews).isNotEqualTo(savedNews);
@@ -178,10 +155,8 @@ class NewsServiceTest {
                 "https://www.naver.com/",
                 true
         );
-
         //when
         ApiResponse<News> response = newsService.createNews(requestDto);
-
         //then
         assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatus());
@@ -199,10 +174,8 @@ class NewsServiceTest {
                 "https://www.naver.com/",
                 true
         );
-
         // when: NewsService의 createNews 메서드를 호출
         ApiResponse<News> response = newsService.createNews(requestDto);
-
         // then: 오류 응답이 반환되는지 확인
         assertNotNull(response);
         assertEquals(ErrorCode.NEWS_IS_EMPTY.getStatus(), response.getStatus()); // 에러 코드 검증
@@ -216,16 +189,12 @@ class NewsServiceTest {
         newsList.add(new News("Test Title1", "Test Source1",  LocalDate.now(), "Test URL1", true));
         newsList.add(new News("Test Title2", "Test Source2",  LocalDate.now(), "Test URL2", true));
         newsList.add(new News("Test Title3", "Test Source3",  LocalDate.now(), "Test URL3", true));
-
         List<News> savedNews = newsList;
-
         // stub
         when(newsRepository.findAll()).thenReturn(savedNews);
-
         // when
         ApiResponse<List<News>> response = newsService.retrieveAllNews();
         List<News> findNews = response.getData();
-
         // then
         Assertions.assertThat(findNews).isEqualTo(savedNews);
         Assertions.assertThat(findNews).hasSize(3);
@@ -236,13 +205,10 @@ class NewsServiceTest {
     void retrieveAllNewsFail() {
         // given
         List<News> newsList = new ArrayList<>();
-
         // stub
         when(newsRepository.findAll()).thenReturn(newsList);
-
         // when
         ApiResponse<List<News>> response = newsService.retrieveAllNews();
-
         // then
         Assertions.assertThat(response.getData()).isNull();
         Assertions.assertThat(response.getMessage()).isEqualTo("News가 존재하지 않습니다.");
@@ -255,11 +221,9 @@ class NewsServiceTest {
         News savedNews = new News("Test Title1", "Test Source1",  LocalDate.now(), "Test URL1", true);
         // stub
         when(newsRepository.findById(id)).thenReturn(Optional.of(savedNews));
-
         // when
         ApiResponse<News> response = newsService.retrieveNewsById(id);
         News findNews = response.getData();
-
         // then
         Assertions.assertThat(findNews).isEqualTo(savedNews);
         Assertions.assertThat(findNews.getTitle()).isEqualTo(savedNews.getTitle());
@@ -277,11 +241,9 @@ class NewsServiceTest {
         News savedNews = new News("Test Title1", "Test Source1",  LocalDate.now(), "Test URL1", true);
         // stub
         when(newsRepository.findById(id)).thenReturn(Optional.empty());
-
         // when
         ApiResponse<News> response = newsService.retrieveNewsById(id);
         News findNews = response.getData();
-
         // then
         Assertions.assertThat(findNews).isNotEqualTo(savedNews);
     }
@@ -294,10 +256,8 @@ class NewsServiceTest {
         News savedNews = new News("Test Title1", "Test Source1", LocalDate.now(), "Test URL1", true);
         // stub
         when(newsRepository.findById(id)).thenReturn(Optional.of(savedNews));
-
         // when
         ApiResponse<String> response = newsService.deleteNews(id);
-
         // then
         Assertions.assertThat(response.getMessage()).isEqualTo("News를 성공적으로 삭제했습니다.");
         // method call verify
@@ -312,10 +272,8 @@ class NewsServiceTest {
         Long id = 1L;
         // stub
         when(newsRepository.findById(id)).thenReturn(Optional.empty());
-
         // when
         ApiResponse<String> response = newsService.deleteNews(id);
-
         // then
         Assertions.assertThat(response.getStatus()).isEqualTo(ErrorCode.INVALID_NEWS_ID.getStatus());
         // method call verify
@@ -330,17 +288,13 @@ class NewsServiceTest {
         newsList.add(new News("Test Title1", "Test Source1",  LocalDate.now(), "Test URL1", true));
         newsList.add(new News("Test Title2", "Test Source2",  LocalDate.now(), "Test URL2", true));
         newsList.add(new News("Test Title3", "Test Source3",  LocalDate.now(), "Test URL3", true));
-
         List<Long> ids = Arrays.asList(1L, 2L, 3L);
-
         // stub
         when(newsRepository.findById(1L)).thenReturn(Optional.of(newsList.get(0)));
         when(newsRepository.findById(2L)).thenReturn(Optional.of(newsList.get(1)));
         when(newsRepository.findById(3L)).thenReturn(Optional.of(newsList.get(2)));
-
         // when
         ApiResponse<String> response = newsService.deleteNewsList(ids);
-
         // then
         Assertions.assertThat(response.getMessage()).isEqualTo("News를 성공적으로 삭제했습니다.");
         // method call verify
@@ -355,14 +309,11 @@ class NewsServiceTest {
         // given
         List<Long> ids = Arrays.asList(1L, 2L);
         News news = new News("Test Title1", "Test Source1", LocalDate.now(), "Test URL1", true);
-
         // stub
         when(newsRepository.findById(1L)).thenReturn(Optional.of(news));
         when(newsRepository.findById(2L)).thenReturn(Optional.empty()); //2번 ID 존재X
-
         // when
         ApiResponse<String> response = newsService.deleteNewsList(ids);
-
         // then
         Assertions.assertThat(response.getStatus()).isEqualTo(ErrorCode.INVALID_NEWS_ID.getStatus());
         Assertions.assertThat(response.getMessage()).isEqualTo(ErrorCode.INVALID_NEWS_ID.getMessage());
