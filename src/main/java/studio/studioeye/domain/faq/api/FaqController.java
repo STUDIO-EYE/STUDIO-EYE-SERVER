1077a90a812da2848677cdaa5175c9b43879166f
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-12-09 15:38:55 +0900

    [REFACTOR]: FaqController 미사용 API 제거

--- src/main/java/studio/studioeye/domain/faq/api/FaqController.java
+++ src/main/java/studio/studioeye/domain/faq/api/FaqController.java
@@ -30,35 +30,10 @@ public class FaqController {
         return faqService.createFaq(dto.toServiceFaq());
     }
 
-    @Operation(summary = "FAQ base64 이미지 저장 후 url 반환 API")
-    @PostMapping("/faq/image")
-    public ApiResponse<String> convertBase64ToImageUrl(@RequestBody String base64Code) throws IOException {
-        return faqService.convertBase64ToImageUrl(base64Code);
-    }
-
     @Operation(summary = "FAQ 전체 조회 API")
     @GetMapping("/faq")
     public ApiResponse<List<Faq>> retrieveAllFaq() {
         return faqService.retrieveAllFaq();
-    }
-
-    @Operation(summary = "FAQ 제목(id, 제목) 전체 조회 API")
-    @GetMapping("/faq/title")
-    public ApiResponse<List<FaqQuestions>> retrieveAllFaqTitle() {
-        return faqService.retrieveAllFaqTitle();
-    }
-
-    @Operation(summary = "id로 FAQ 상세 조회 API")
-    @GetMapping("/faq/{id}")
-    public ApiResponse<Faq> retrieveFaqById(@PathVariable Long id) {
-        return faqService.retrieveFaqById(id);
-    }
-
-    @Operation(summary = "FAQ 페이지네이션 조회 API")
-    @GetMapping("/faq/page")
-    public Page<Faq> retrieveFaqPage(@RequestParam(defaultValue = "0") int page,
-                                     @RequestParam(defaultValue = "10") int size) {
-        return faqService.retrieveFaqPage(page, size);
     }
 
     @Operation(summary = "FAQ 수정 API")
@@ -71,11 +46,5 @@ public class FaqController {
     @DeleteMapping("/faq/{id}")
     public ApiResponse<String> deleteFaq(@PathVariable Long id) {
         return faqService.deleteFaq(id);
-    }
-
-    @Operation(summary = "id로 FAQ 복수 삭제 API")
-    @DeleteMapping("/faq")
-    public ApiResponse<String> deleteFaqs(@RequestBody List<Long> ids) {
-        return faqService.deleteFaqs(ids);
     }
 }
