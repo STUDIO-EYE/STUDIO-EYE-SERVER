ecd7ea125a3fcb6a9bea0febf3a7a2edcbf7f090
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-12-09 15:20:56 +0900

    [REFACTOR]: 사용하지 않는 DTO 제거

--- src/main/java/studio/studioeye/domain/menu/dto/request/UpdateMenuServiceRequestDto.java
+++
@@ -1,8 +0,0 @@
-package studio.studioeye.domain.menu.dto.request;//package studio.studioeye.domain.menu.dto.request;
-//
-//public record UpdateMenuServiceRequestDto(
-//        Long id,
-//        Boolean visibility,
-//        Integer order
-//) {
-//}
